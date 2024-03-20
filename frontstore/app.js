9e04e21f85315fa90b39d150eb63229784bbb67b
Tinnagame <98tinnagame@gmail.com>
2024-03-21 02:20:36 +0700

    include map api // no config

--- frontstore/app.js
+++ frontstore/app.js
@@ -7,6 +7,15 @@ const db = require("./config/db");
 //listItem.create(newItem);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.set("view engine", "ejs");
 
@@ -29,6 +38,10 @@ app.get("/game/:id", async function (req, res) {
   res.render("information-page/information-page", {
     gameInfo: gameInfo,
   });
+});
+
+app.get("/contact-us", async function (req, res) {
+  res.render("contact-page/contact-page");
 });
 
 app.listen("3000", () => {
---
+++ frontstore/config/map.js
@@ -0,0 +1,3 @@
+apiKey = "1f9627ed38e4991a82ad697ded786f51";
+
+module.exports = apiKey;
